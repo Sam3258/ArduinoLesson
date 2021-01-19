a03abfa12b13bc8664607ee113971a40d3e32b1f
Sam Sharing <Sam3258@users.noreply.github.com>
2021-01-19 10:05:07 +0800

    upload source code (.ino) for lesson 2

---
+++ Lesson 2/lesson2.ino
@@ -0,0 +1,16 @@
+/* 山姆來分享 Lesson 2 */
+
+vode setup() {
+	pinMode(12, OUTPUT);			// initial digital pin-12 (LED1) as an output
+	pinMode(13, OUTPUT);			// initial digital pin-13 (LED2) as an output
+}
+
+void loop() {
+	digitalWrite(12, HIGH);			// turn the LED1 on
+	delay(1000);					// wait for a second
+	digitalWrite(12, LOW);			// turn the LED1 off
+	
+	digitalWrite(13, HIGH);			// turn the LED2 on
+	delay(1000);					// wait for a second
+	digitalWrite(13, LOW);			// turn the LED2 off
+}	
